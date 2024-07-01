54332f5a679026e0ea65f50ea49f6e5ab91c7060
Juan <juanmanueljoaquin@hotmail.com>
2024-07-01 16:15:07 -0300

    update link css

--- src/components/Cursos/InscriptionPage/InscriptionPage.jsx
+++ src/components/Cursos/InscriptionPage/InscriptionPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import "../InscriptionPage/InscriptionStyle.css"
+import './InscriptionStyle.css';
 
 
 const InscriptionPage = () => {
